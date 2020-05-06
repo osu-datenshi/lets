2b8bc282663eaf8e1e1ac99aacd52e8247307d2b
troke12 <ochi@troke.id>
2020-05-07 06:50:40 +0800

    fix replay relax doesnt serving

--- handlers/getReplayHandler.py
+++ handlers/getReplayHandler.py
@@ -50,19 +50,25 @@ class handler(requestsManager.asyncRequestHandler):
 			# Get user ID
 			replayData = glob.db.fetch("SELECT scores.*, users.username AS uname FROM scores LEFT JOIN users ON scores.userid = users.id WHERE scores.id = %s", [replayID])
 			if replayData == None:
-				replayData = glob.db.fetch("SELECT scores_relax.*, users.username AS uname FROM scores_relax LEFT JOIN users ON scores_relax.userid = users.id WHERE scores_relax.id = %s", [replayID])
-				fileName = "{}_relax/replay_{}.osr".format(glob.conf.config["server"]["replayspath"], replayID)
-				UsingRelax = True
+				replayData = glob.db.fetch("SELECT scores.*, users.username AS uname FROM scores LEFT JOIN users ON scores.userid = users.id WHERE scores.id = %s", [replayID])
+				fileName = ".data/replays/replay_{}.osr".format(replayID)
+				UsingRelax = False
 			else:
-				fileName = "{}/replay_{}.osr".format(glob.conf.config["server"]["replayspath"], replayID)
+				fileName = ".data/replays_relax/replay_{}.osr".format(replayID)
+				UsingRelax = True
 
 			# Increment 'replays watched by others' if needed
 			if replayData is not None:
 				if username != replayData["uname"]:
 					userUtils.incrementReplaysWatched(replayData["userid"], replayData["play_mode"])
-
 			# Serve replay
-			log.info("[{}] Serving replay_{}.osr".format("RELAX" if UsingRelax else "VANILLA", replayID))
+
+			if UsingRelax:
+				Play = "RELAX"
+			else:
+				Play = "VANILLA"
+
+			log.info("[{}] Serving replay_{}.osr".format(Play, replayID))
 
 			if os.path.isfile(fileName):
 				with open(fileName, "rb") as f:
