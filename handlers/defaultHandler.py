2da29a960ba7738a056298c1bd47acea9d648c75
unknown <jame_glove@yahoo.com>
2020-09-24 16:12:49 +0800

    edit default

--- handlers/defaultHandler.py
+++ handlers/defaultHandler.py
@@ -10,21 +10,5 @@ class handler(requestsManager.asyncRequestHandler):
 	def asyncGet(self):
 		print("404: {}".format(self.request.uri))
 		self.write("""
-				<html>
-					<head>
-						<link href="https://i.datenshi.xyz/static/memez.css" media="all" rel="stylesheet">
-						<link href="https://goverit.troke.id/uikit.css" media="all" rel="stylesheet">
-					</head>
-					<body class="uk-flex uk-flex-column uk-flex-middle uk-flex-between uk-light">
-					<div></div>
-					<div class="uk-flex uk-flex-column uk-flex-middle uk-margin-large-bottom">
-					<img src="https://datenshi.xyz/static/logo.png" class="uk-margin-small-bottom" width="300" height="300">
-					<div class="uk-h1 uk-text-uppercase uk-margin-remove">DATENSHI</div>
-					<div class="uk-h5 uk-text-uppercase uk-margin-remove-bottom">First Indonesian osu! Private Server</div>
-					<div class="uk-h5 uk-text-uppercase uk-margin-remove">Enjoy, Come and Join Us</div>
-					<div class="uk-h5 uk-text-uppercase uk-margin-remove-bottom"><a href="https://link.troke.id/datenshi" target="_blank" class="uk-text-bold">DISCORD</a></div>
-					</div>
-					<div></div>
-					</body>
-				</html>
+				<!DOCTYPE html> <head> <meta charset='utf-8'> <meta http-equiv='X-UA-Compatible' content='IE=edge'> <title>DATENSHI</title> <meta name='description' content='DATENSHI BACKEND'> <meta name='viewport' content='width=device-width, initial-scale=1'> <link rel='icon' href='https://i.datenshi.xyz/static/logos/text-white.png'> <style>body{background-color: blueviolet;}.img-container{text-align: center; display: block;}h1, p, a{color: aliceblue; font-family: Arial, Helvetica, sans-serif;}</style> </head><body> <div class='img-container'> <img src='https://datenshi.xyz/static/logo2.png'> <h1>DATENSHI</h1> <p>So you are here? If you do understand exactly what you are doing, you should come work with us <a href='https://github.com/osu-datenshi'>/github</a></p></div></body></html>
 				""")
