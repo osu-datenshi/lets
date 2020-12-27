c6fbcc6567ef23f11bbe31ff3d0d38e9e69de872
unknown <jame_glove@yahoo.com>
2020-12-27 17:53:29 +0800

    testing dummy part 5.1

--- lets.py
+++ lets.py
@@ -322,7 +322,7 @@ def ping():
     # PING
 	try:
 		glob.db.execute("SELECT 1+1")
-        consoleHelper.printColored("the command has been execute!", bcolors.GREEN)
+    	consoleHelper.printColored("the command has been execute!", bcolors.GREEN)
 	except:
 		consoleHelper.printColored("command not working", bcolors.RED)
 
